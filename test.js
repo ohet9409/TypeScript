a4d1efb10c4c6f2dee77322f042c6d6ce5afda70
ohet9409 <dhdmsxor1@naver.com>
2021-06-10 22:02:31 +0900

    타입스크립트 시작 boolean

---
+++ test.js
@@ -0,0 +1,10 @@
+"use strict";
+var a = 'Mark';
+// a = 39;
+var b;
+b = 'String';
+// b= 00;
+function hello(b) {
+}
+// hello('String')
+hello(99);
---
+++ test.ts
@@ -0,0 +1,16 @@
+let a = 'Mark';
+
+// a = 39;
+
+let b: String;
+
+b = 'String';
+
+// b= 00;
+
+function hello(b: number){
+
+}
+
+// hello('String')
+hello(99)
