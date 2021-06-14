a1131ea7910847db5968a361c2ac912c8bd99fb2
ohet9409 <dhdmsxor1@naver.com>
2021-06-14 22:58:17 +0900

    null, undefined

---
+++ null.ts
@@ -0,0 +1,26 @@
+
+//  null 사용 불가
+// let Myname: string = null;
+
+//  let u: undefined = null;
+
+let v: void = undefined;
+
+// string 타입과 null을 함께 지정
+let union: string | null = null;
+
+union = "Mark";
+
+// ===============================
+// null
+let n: null = null;
+
+console.log(n); // null
+console.log(typeof n); // object
+
+// ================================
+// undefined
+let u: undefined = undefined;
+
+console.log(u); // undefined
+console.log(typeof u); // undefined
